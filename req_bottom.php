58c62de5d26d4f4f6c4a9e764e7ced88ffd02c63
Roberto Marzocchi <gter@gter.it>
2021-02-04 21:29:29 +0100

    gitignore

---
+++ req_bottom.php
@@ -0,0 +1,62 @@
+<?php 
+$subtitle2=str_replace("'","\'",str_replace(' ','_',$subtitle));
+?>
+
+
+
+
+
+	<!-- Bootstrap core JS  (da mettere poi in locale)-->
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+	<!-- Third party plugin JS-->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+	<!-- Core theme JS-->
+	<script src="js/scripts.js"></script>
+		
+
+
+	<script src="./bootstrap-table/dist/bootstrap-table.js" ></script>
+	<script src="./bootstrap-table/dist/extensions/export/bootstrap-table-export.js" ></script> 
+	<script src="./bootstrap-table/dist/extensions/print/bootstrap-table-print.min.js" ></script> 
+	<script src="./bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.js" ></script> 
+	<!--script src="//rawgit.com/hhurz/tableExport.jquery.plugin/master/tableExport.js"></script-->
+	<!--script src="./bootstrap_plugin_export/tableExport.js"></script-->
+	<script type="text/javascript" src="./bootstrap-validator-0.11.9/dist/validator.min.js"></script>
+
+
+
+
+	<!-- OLD DA CONTROLLARE IN CASO DI PROBLEMI -->
+
+	 <!-- jQuery -->
+	<!--script src="js/jquery.js"></script-->
+
+	<!-- Bootstrap Core JavaScript -->
+	<!--script src="js/bootstrap.js"></script-->
+
+	<!-- Plugin JavaScript -->
+	<!--script src="js/jquery.easing.min.js"></script>
+	<script src="js/jquery.fittext.js"></script>
+	<script src="js/wow.min.js"></script-->
+
+	<!-- Custom Theme JavaScript -->
+	<!--script src="js/creative.js"></script-->
+
+	<!-- jQuery -->
+	<!--script src="vendor/scrollreveal/scrollreveal.js"></script>
+	<script src="vendor/magnific-popup/jquery.magnific-popup.js"></script>
+	
+	<script src="js/creative.js"></script-->  
+
+	
+
+	
+
+
+<?php
+// end
+?>
+    
